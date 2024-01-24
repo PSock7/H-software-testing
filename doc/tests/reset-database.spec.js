be7ab471db095dda984bb4428803a3fb522c24e4
xDev <papesock@yahoo.fr>
2024-01-24 11:36:33 +0100

    reset database test

---
+++ doc/tests/reset-database.spec.js
@@ -0,0 +1,16 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Database management', () => {
+  
+  test('should reset the database', async ({ page }) => {
+    
+    await page.goto('https://h.hr.dmerej.info/'); 
+    await page.click('text=Reset database');
+    await expect(page).toHaveURL('https://h.hr.dmerej.info/reset_db');
+
+    await expect(page.locator('text=You are about to delete all the information from the database.')).toBeVisible();
+    await page.click('button.btn-danger[type="submit"]');
+    
+  });
+
+});
