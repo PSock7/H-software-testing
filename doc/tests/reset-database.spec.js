8db9f2f3caf740696eac1d828ba131e77a7c7d42
xDev <papesock@yahoo.fr>
2024-01-24 16:39:30 +0100

    journal log

--- doc/tests/reset-database.spec.js
+++ doc/tests/reset-database.spec.js
@@ -1,16 +1,16 @@
-const { test, expect } = require('@playwright/test');
+  const { test, expect } = require('@playwright/test');
 
-test.describe('Database management', () => {
-  
-  test('should reset the database', async ({ page }) => {
+  test.describe('Database management', () => {
     
-    await page.goto('https://h.hr.dmerej.info/'); 
-    await page.click('text=Reset database');
-    await expect(page).toHaveURL('https://h.hr.dmerej.info/reset_db');
+    test('should reset the database', async ({ page }) => {
+      
+      await page.goto('https://h.hr.dmerej.info/'); 
+      await page.click('text=Reset database');
+      await expect(page).toHaveURL('https://h.hr.dmerej.info/reset_db');
 
-    await expect(page.locator('text=You are about to delete all the information from the database.')).toBeVisible();
-    await page.click('button.btn-danger[type="submit"]');
-    
+      await expect(page.locator('text=You are about to delete all the information from the database.')).toBeVisible();
+      await page.click('button.btn-danger[type="submit"]');
+      
+    });
+
   });
-
-});
